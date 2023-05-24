5848213346b80e660a7ca76ea197105b783c4a90
_tango <shurinskiy@mail.ru>
2023-05-24 18:12:07 +0600

    start commit

--- src/js/blocks.js
+++
@@ -1,6 +0,0 @@
-
-import "../blocks/feed/feed.js";
-import "../blocks/header/header.js";
-import "../blocks/hero/hero.js";
-import "../blocks/partners/partners.js";
-import "../blocks/rating/rating.js";
