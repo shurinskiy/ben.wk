7e3294285a7cb930686adfd8820f53717b22e8ce
_tango <shurinskiy@mail.ru>
2023-05-24 18:14:51 +0600

    add files

---
+++ src/js/blocks.js
@@ -0,0 +1,6 @@
+
+import "../blocks/feed/feed.js";
+import "../blocks/header/header.js";
+import "../blocks/hero/hero.js";
+import "../blocks/partners/partners.js";
+import "../blocks/rating/rating.js";
